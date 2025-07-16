bcf0396a0f5e51c3715c66be9c5a7e5d0d76a422
danu03 <danusuhendra03@gmail.com>
2025-07-16 11:28:06 +0700

    Initial Commit

---
+++ Claim/ClaimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaimApp.swift
+//  Claim
+//
+//  Created by Danu  on 16/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ClaimListView()
+        }
+    }
+}
---
+++ Claim/ClaimListView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Claim
+//
+//  Created by Danu  on 16/7/25.
+//
+
+import SwiftUI
+
+struct ClaimListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ClaimListView()
+}
